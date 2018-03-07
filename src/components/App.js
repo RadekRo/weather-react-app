8df0ebb32177f8f118b426c2903c5353762d30b9
Radosław Rocławski <r.roclawski@ankara.pl>
2018-03-07 08:11:40 +0100

    Created new react app. Deleted unused files. Created temp. message in App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          Weather Forecast Application
+      </div>
+    );
+  }
+}
+
+export default App;
